5b55f353a2bcfebc3dfb2f1c215acd13916d74ba
nanggalasakti54 <42232103+nanggalasakti54@users.noreply.github.com>
2022-04-28 16:35:57 +0800

    update home

---
+++ app/Views/menu.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="header">
+            PROFIL MAHASISWA
+            <div class="logoheader">
+                <img src="img/logoundiknas.jpeg" alt="" width="80px" height="80px">
+            </div>
+        </div>
+        <div class="content">
+            <div class="contimg1">
+                <img src="img/bg.JPG" class="img1">
+                <div class="img2">
+                    <a href="https://www.facebook.com/profile.php?id=100006768016538"> <img class="div.img" src="img/fb.jpg" width="70px" height="70px"></a>
+                    <a href="https://www.instagram.com/gunturnanggala/"> <img class="div.img" src="img/ig.jpg" width="70px" height="70px"></a>
+                    <a href="https://twitter.com/nanggala_sakti"><img class="div.img" src="img/twit.jpg" width="70px" height="70px"></a>
+                </div>
+            </div>
+            <div class="contimg2">
+                <div class="text1">
+                    <h1>Intro</h1>
+                    <p>
+                        Nama saya Guntur Nanggala Sakti, saya adalah mahasiswa di salah satu Universitas ternama di Bali. Universitas Pendidikan Nasional adalah tempat dimana saya menimba ilmu pendidikan tingkat lanjut, setelah saya merantau di Bali
+                    </p>
+                </div>
+                <br>
+                <div class="text2">
+                    <h2>Pendidikan</h2>
+                    <ul>
+                        <li>SD Negeri 1 Pekuncen</li>
+                        <li>SMP Negeri 2 Ajibarang</li>
+                        <li>SMK Telkom Purwokerto</li>
+                    </ul>
+                </div>
+                <div class="text1">
+                    <h2>Pengalaman Kerja</h2>
+                    <p>
+                        Pada saat SMK saya pernah melakukan kegiatan PKL atau sering disebut dengan Praktek Kerja Lapangan, saya ditempatkan pada devisi Teknologi Informasi. saya belajar bagaimana cara membuat sebuah jaringan dan melakukan pengembangan jaringan disana.
+                    </p>
+                </div>
+            </div>
+            <div class="contimg3">
+                <div class="text1">
+                    <h2>Hobby</h2>
+                    <p>
+                        Saya adalah manusia yang senang dengan kegiatan fisik, seperti olahraga, hiking, dan kegiatan aktifitas dialam. karena dengan melakukan aktifitas itu saya bisa merasa lebih baik, walau terkadang saya melakukannya sendiri, apalagi kegiatan dialam, saya
+                        tak pernah merasa bosan, karena disana banyak sekali hal yang bisa membuat hati terasa senang bahkan dapat meningkatkan mood.
+                    </p>
+                </div>
+                <img src="img/mt.JPG" class="img3">
+            </div>
+        </div>
+        <div class="footer"></div>
+    </div>
+</body>
+
+</html>
